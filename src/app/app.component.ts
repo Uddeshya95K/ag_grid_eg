4417e2a0a178431f4655b0ead91822918dcebb0b
Uddeshya <41189811+Nivasoftdeveloper1@users.noreply.github.com>
2019-11-26 11:22:15 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,79 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'swayaan';
+  public gridOptions;
+  public gridApi;
+  public gridColumnApi;
+  public columnDefs;
+  public rowData;
+  private rowSelection;
+
+  constructor(private http: HttpClient) {
+    this.columnDefs = [
+      {headerName: 'ID', field: 'id'},
+      {headerName: 'Name', field: 'name', editable: true},
+      {headerName: 'Year', field: 'year',  editable: true},
+      {headerName: 'Color', field: 'color', editable: true},
+      {headerName: 'Pantone Value', field: 'pantone_value',  editable: true}
+    ],
+    this.rowData = [],
+    this.rowSelection = 'single'
+  }
+
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+
+    this.http
+      .get('https://reqres.in/api/posts')
+      .subscribe(r => {
+        this.rowData = r["data"];
+      });
+
+    // this method is for get data and do get api call
+  }
+
+  onBtAdd(){
+    this.http
+      .post('https://reqres.in/api/posts', 
+      {
+        "name": "Uddeshya",
+        "year": 2017,
+        "color": "#67HG88",
+        "pantone_value": "87-GHH"
+      })
+      .subscribe(r => {
+        delete r["createdAt"];
+        this.gridApi.updateRowData({ add: [r] });
+      });
+
+    // this method is for add row in the list and do post api call to insert data
+  }
+
+  onBtRemove(){
+    var selectedRows = this.gridApi.getSelectedRows();
+    this.gridApi.updateRowData({ remove: selectedRows });
+
+    // this method is for remove row in the list to delete data.
+    // And i tried for api call but the responce in postman 204 coming.
+
+  }
+
+  onCellValueChanged(params) {
+    const colId = params.column.getId();
+    const newValue = params.newValue;
+    const index = params.rowIndex;
+
+    this.http
+      .put('https://reqres.in/api/posts/597', this.rowData[index])
+      .subscribe(r => {
+        delete r["updatedAt"];
+        this.rowData[index][colId] = newValue;
+      });
+
+    // this method is for update row in the list and do put api call to update data.
+    // You will able to update/edit on double click on row cell and press enter
+  }
+
+  // to do this all operation we using ag-grid to show list and do put, post, get operation
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { AgGridModule } from './ag-grid-community';
+import { AgGridModule } from 'ag-grid-angular';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AgGridModule.withComponents(null),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
